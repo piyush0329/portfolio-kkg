bcc7928afa39639e0335e88a2e8db58f89d1493d
piyush0329 <upadhyaypiyush29@gmail.com>
2023-04-18 00:26:25 +0530

    content added

--- form_handler.php
+++ form_handler.php
@@ -11,7 +11,7 @@ $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n". 
                   "User Message: $message.\n";
 
-$to ='kkgola1503@gmail.com';
+$to ='kkgolaa1503@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
@@ -20,4 +20,5 @@ $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: index.html");
+echo 'alert("message sent sucessfully")';
 ?>
