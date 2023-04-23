ba1ef327f7e1a9940c7b8e4157a7ebcf8bd7a3a1
Tushar Srivastava <tusharsrivastava2904@gmail.com>
2023-04-23 19:19:07 +0530

    multi charts added

--- citation_script.js
+++ citation_script.js
@@ -1,9 +1,6 @@
 // script for citation
-var citations_chart = null;
 
-var canvasElement = document.getElementById("citations").getContext("2d");
-
-var config = {
+var configAll = {
   type: "bar",
   data: {
     labels: [
@@ -56,17 +53,17 @@ var config = {
         label: "Scopus Citations",
         data: [0, 0, 0, 5, 5, 2, 5, 4, 20, 62, 18],
         backgroundColor: [
-          "rgba(255, 255, 79, 0.3)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
-          "rgba(255, 255, 255, 0.5)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
         ],
         borderColor: [
           "rgba(255, 255, 255, 1)",
@@ -119,9 +116,16 @@ var config = {
     ],
   },
   options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "#eeeeee",
+        },
+      },
+    },
     maintainAspectRatio: false,
     animation: {
-        duration: 1000,
+      duration: 1000,
     },
     scales: {
       y: {
@@ -133,9 +137,9 @@ var config = {
           stepSize: 25,
         },
         title: {
-            display: true,
-            text: 'Number of Citations',
-            color: '#808080'
+          display: true,
+          text: "Number of Citations",
+          color: "#aaaaaa",
         },
       },
       x: {
@@ -145,15 +149,162 @@ var config = {
         title: {
           display: true,
           text: "Year",
-          color: '#808080'
+          color: "#aaaaaa",
         },
       },
     },
   },
 };
 
+var configGScholar = {
+  type: "bar",
+  data: {
+    labels: [
+      "Citations",
+      "h-index",
+      "i10-index",
+    ],
+    datasets: [
+      {
+        label: "Google Scholar Citations",
+        data: [271, 9, 8],
+        backgroundColor: "rgba(255, 0, 79, 0.3)",
+        borderColor: "rgba(255, 0, 79, 1)",
+        borderWidth: 2,
+        hoverBorderWidth: 5,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "#eeeeee",
+        },
+      },
+    },
+    maintainAspectRatio: false,
+    animation: {
+      duration: 1000,
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          color: "#262626",
+        },
+        ticks: {
+          stepSize: 25,
+        },
+        title: {
+          display: true,
+          text: "Number of Citation",
+          color: "#aaaaaa",
+        },
+      },
+      x: {
+        grid: {
+          color: "#262626",
+        },
+        title: {
+          display: true,
+          text: "Type of Citations",
+          color: "#aaaaaa",
+        },
+        ticks: {
+          color: '#eeeeee'
+        }
+      },
+    },
+  },
+};
+
+var configScopus = {
+  type: "bar",
+  data: {
+    labels: ["Citations", "h-index", "Documents"],
+    datasets: [
+      {
+        label: "Scopus Citations and Documents",
+        data: [121, 5, 33],
+        backgroundColor: "rgba(255, 0, 79, 0.3)",
+        borderColor: "rgba(255, 0, 79, 1)",
+        borderWidth: 2,
+        hoverBorderWidth: 5,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "#eeeeee",
+        },
+      },
+    },
+    maintainAspectRatio: false,
+    animation: {
+      duration: 1000,
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          color: "#262626",
+        },
+        ticks: {
+          stepSize: 25,
+        },
+        title: {
+          display: true,
+          text: "Number of Citation",
+          color: "#aaaaaa",
+        },
+      },
+      x: {
+        grid: {
+          color: "#262626",
+        },
+        title: {
+          display: true,
+          text: "Type of Citations",
+          color: "#aaaaaa",
+        },
+        ticks: {
+          color: "#eeeeee",
+        },
+      },
+    },
+  },
+};
+
+// initialization
+var citations_chart = null;
+var canvasElement = document.getElementById("citations").getContext("2d");
+
+//default
+let chartType = document.getElementById('selector').value;
 if (citations_chart != null) {
   citations_chart.destroy();
-}
+} 
+citations_chart = new Chart(canvasElement, configAll);
 
-citations_chart = new Chart(canvasElement, config);
+document.getElementById('selector').addEventListener('change', ()=>{
+  chartType = document.getElementById('selector').value;
+  if (chartType==='all'){
+    if (citations_chart != null) {
+      citations_chart.destroy();
+    }
+    citations_chart = new Chart(canvasElement, configAll);
+  } else if (chartType==='scholar'){
+    if (citations_chart != null) {
+      citations_chart.destroy();
+    }
+    citations_chart = new Chart(canvasElement, configGScholar);
+  } else if (chartType==='scopus'){
+    if (citations_chart != null) {
+      citations_chart.destroy();
+    }
+    citations_chart = new Chart(canvasElement, configScopus);
+  }
+});
